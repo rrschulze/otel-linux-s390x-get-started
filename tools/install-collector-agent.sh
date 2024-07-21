b6ad09f5449aeeac4db0c0ba522e10926a9e834d
Ruediger Schulze <ruediger.schulze@de.ibm.com>
2024-07-21 19:26:00 +0200

    added contrib collector to installation

--- tools/install-collector-agent.sh
+++ tools/install-collector-agent.sh
@@ -4,6 +4,8 @@
 mkdir bin
 cd bin
 curl --proto '=https' --tlsv1.2 -fOL https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.105.0/otelcol_0.105.0_linux_s390x.tar.gz
+curl --proto '=https' --tlsv1.2 -fOL https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.105.0/otelcol-contrib_0.105.0_linux_s390x.tar.gz
 tar -xvf otelcol_0.105.0_linux_s390x.tar.gz
+tar -xvf otelcol-contrib_0.105.0_linux_s390x.tar.gz
 cd ..
-./bin/otelcol --config=./config/collector/agent.yml
+./bin/otelcol-contrib --config=./config/collector/agent.yml
