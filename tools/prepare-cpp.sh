e86d3c8f9c69f625186e02f6cda6a6452e77560c
Ruediger Schulze <ruediger.schulze@de.ibm.com>
2024-07-21 14:18:14 +0200

    Multiple fixes
    Fixed agent configuration
    Clean up of gateway configuration
    Fixed metrics issue

--- tools/prepare-cpp.sh
+++ tools/prepare-cpp.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-
 # requirements for building open-telemetry-cpp
-sudo apt -y install libprotobuf-dev protobuf-compiler
-sudo apt -y install curl
-sudo apt -y install libcurl4-openssl-dev
+apt -y install libprotobuf-dev protobuf-compiler
+apt -y install curl
+apt -y install libcurl4-openssl-dev
+
+apt -y install libgtest-dev
+cd /usr/src/googletest
+cmake .
+cmake --build . --target install
+
+git clone https://github.com/google/benchmark.git
+mkdir benchmark-build
+cd benchmark-build
+cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/../benchmark-install ../benchmark
+make install
