578a57a014347a587a2c068b45b01d680bd75b98
Ruediger Schulze <ruediger.schulze@de.ibm.com>
2024-07-21 19:15:29 +0200

    Added install collector agent

---
+++ tools/install-collector-agent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# download OTel collector
+mkdir bin
+cd bin
+curl --proto '=https' --tlsv1.2 -fOL https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.105.0/otelcol_0.105.0_linux_s390x.tar.gz
+tar -xvf otelcol_0.105.0_linux_s390x.tar.gz
+cd ..
+./bin/otelcol --config=./config/collector/agent.yml
